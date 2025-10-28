380f20b50384e074055656ee7801250a971f8315
vivekhaldkarkte <vivekhaldkarkte@gmail.com>
2025-10-28 13:44:19 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.17",
   networks: {
-    coreTestnet: {
+    coreTestnet:{
       url: "https://rpc.test2.btcs.network",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []  ,
       chainId: 1114,
@@ -21,6 +21,7 @@ module.exports = {
     tests:"./test",
   },
 };
+
 
 
 
